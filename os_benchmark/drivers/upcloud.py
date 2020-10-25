3b4cb17d47f11d96e57acd67c9d48705b8fe5015
Anthony Monthe <anthony.monthe@gmail.com>
2020-10-25 16:31:29 +0000

    Added UpCloud

---
+++ os_benchmark/drivers/upcloud.py
@@ -0,0 +1,33 @@
+"""
+.. note::
+  This driver requires `boto3`_.
+
+Configuration
+~~~~~~~~~~~~~
+
+.. code-block:: yaml
+
+  ---
+  upcloud:
+    driver: upcloud
+    aws_access_key_id: <your_ak>
+    aws_secret_access_key: <your_sk>
+
+.. _boto3: https://github.com/boto/boto3
+"""
+from urllib.parse import urlparse
+from os_benchmark.drivers import s3
+
+
+class Driver(s3.Driver):
+    """UpCloud S3 Driver"""
+    id = 'upcloud'
+
+    def get_custom_kwargs(self, kwargs):
+        kwargs['endpoint_url'] = self.kwargs['endpoint_url']
+        return kwargs
+
+    def get_url(self, bucket_id, name, **kwargs):
+        domain = urlparse(self.kwargs['endpoint_url']).netlooc
+        url = 'https://%s.%s/%s' % (bucket_id, domain, name)
+        return url
