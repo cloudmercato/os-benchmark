61907e8bef74d37ec20216442911e4e3fa86ca85
Anthony Monthe <anthony.monthe@gmail.com>
2022-03-10 05:33:30 +0000

    Oops the logger !

---
+++ os_benchmark/logger.py
@@ -0,0 +1,10 @@
+import logging
+
+formatter = logging.Formatter('[%(asctime)s %(levelname)s]: %(message)s')
+
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+
+logger = logging.getLogger('osb')
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
