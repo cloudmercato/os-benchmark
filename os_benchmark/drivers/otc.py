47654825a4a7f5585dfa56fbf87d2e8ad51871e3
Anthony Monthe <anthony.monthe@gmail.com>
2022-03-19 03:24:33 +0000

    Updated OTC for eu-nl

--- os_benchmark/drivers/otc.py
+++ os_benchmark/drivers/otc.py
@@ -12,6 +12,12 @@ Configuration
     driver: otc
     aws_access_key_id: <your_ak>
     aws_secret_access_key: <your_sk>
+    endpoint_url: https://obs.<zone_id>.otc.t-systems.com
+
+Possible zone IDs are the following:
+
+- ``eu-de``: Germany
+- ``eu-nl``: Netherland
 
 .. _boto3: https://github.com/boto/boto3
 """
@@ -21,11 +27,15 @@ from os_benchmark.drivers import s3
 class Driver(s3.Driver):
     """T-Systems Open Cloud Telekom S3 Driver"""
     id = 'otc'
-    endpoint_url = 'https://obs.otc.t-systems.com'
-    default_kwargs = {
-        'endpoint_url': endpoint_url,
-    }
+
+    def _get_create_request_params(self, *args, **kwargs):
+        params = super()._get_create_request_params(*args, **kwargs)
+        if 'region_name' in self.kwargs:
+            params['CreateBucketConfiguration'] = {
+                'LocationConstraint': self.kwargs['region_name']
+            }
+        return params
 
     def get_url(self, bucket_id, name, **kwargs):
-        url = '%s/%s/%s' % (self.default_kwargs['endpoint_url'], bucket_id, name)
+        url = '%s/%s/%s' % (self.kwargs['endpoint_url'], bucket_id, name)
         return url
