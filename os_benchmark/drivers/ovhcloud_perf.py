f0589a256c340305e40ed5d631549d45b77c754a
Anthony Monthe <anthony.monthe@gmail.com>
2024-10-30 23:09:07 +0000

    Update ovhcloud_perf.py

--- os_benchmark/drivers/ovhcloud_perf.py
+++ os_benchmark/drivers/ovhcloud_perf.py
@@ -24,6 +24,7 @@ from os_benchmark.drivers import s3
 class Driver(s3.Driver):
     """High Performance Object Storage Driver"""
     id = 'ovhcloud_perf'
+    default_object_acl = 'public-read'
 
     def get_url(self, bucket_id, name, **kwargs):
         netloc = urlparse(self.kwargs['endpoint_url']).netloc
